ebf84a4372271f7e8e285587e7bd3f933aa95f4a
Rasa <r.valentinaviciute@uea.ac.uk>
2017-11-26 00:00:12 +0000

    Added the switch between views function in booking

--- web/HHjavascript.js
+++ web/HHjavascript.js
@@ -5,15 +5,30 @@
  */
 
 function switchToManage(){
-    var manageBooking = {Name: "Manage a Booking"};
+    var manageBooking = {
+        Name: "Manage a Booking"
+    };
     
     $('#bookingTitle').empty();
     $('#bookingTitle').append(manageBooking.Name);
+    
+    document.getElementById('book').style.display='none';
+    document.getElementById('manage').style.display='block';
 }
 
 function switchToBook(){
-    var makeBooking = {Name: "Make a Booking"};
+    var makeBooking = {
+            Name: "Make a Booking"
+    };
     
     $('#bookingTitle').empty();
     $('#bookingTitle').append(makeBooking.Name);
+    
+    document.getElementById('manage').style.display='none';
+    document.getElementById('book').style.display='block';
 }
+
+$(document).ready(function(){
+   $("#manage").hide();
+
+});
