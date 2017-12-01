f9e91dcc9aae963c7997b97035b056b93e1b0256
Rasa <r.valentinaviciute@gmail.com>
2017-12-01 18:22:50 +0000

    Adding the sql

---
+++ src/java/AccessDB/AccessServlet.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccessDB;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author fvq13ndu
+ */
+public class AccessServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try {
+            String insertSQL;
+
+            insertSQL = "insert into customer values (123470, 'Ann Hinchcliffe14', 'Ann.Hinchcliffe@yahoo.com', '81 New Road, Acle NR13 7GH', 'V', '10/16', '8948106927123585');";
+            System.out.println(insertSQL);
+
+            String cmpHost = "cmpstudb-02.cmp.uea.ac.uk:5432";
+            String myDbName = "groupdk"; //your DATABASE name, same as your username 
+            String myDBusername = "groupdk"; // use your username for the database username  
+            String myDBpwd = "groupdk"; // use your DB’s password, same as your username  
+
+            // make a string for my DB’s connection url 
+            String myDbURL = ("jdbc:postgresql://" + cmpHost + "/" + myDbName);
+
+            Class.forName("org.postgresql.Driver");
+            // connect to my database on CMP’s web server.
+            Connection connection = DriverManager.getConnection(myDbURL, myDBusername, myDBpwd);
+            Statement statement = connection.createStatement();
+//                statement.executeUpdate(insertSQL);
+            statement.execute("set schema 'HeartacheHotelDB';");
+            statement.execute(insertSQL);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error: " + e);
+        }
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
